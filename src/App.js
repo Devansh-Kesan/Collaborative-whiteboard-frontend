5eb8136922b48685e44c9f6974e0a056bc1e8f10
Devansh Kesan <devanshkesan101@gmail.com>
2025-07-24 19:48:34 +0530

    Combined all the components into one App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Board from "./components/Board";
+import Toolbar from "./components/Toolbar";
+import Toolbox from "./components/Toolbox";
+import Sidebar from "./components/Sidebar";
+import BoardProvider from "./store/BoardProvider";
+import ToolboxProvider from "./store/ToolboxProvider";
+import Login from "./components/Login";
 import Register from "./components/Register";
+import { useParams } from "react-router-dom";
 
 
-
+function HomePage() {
+  const { id } = useParams(); // Get the dynamic id
+  return (
+    <ToolboxProvider>
+      <div className="app-container">
+        <Toolbar />
+        <Board id={id}/>
+        <Toolbox />
+        <Sidebar /> 
+      </div>
+    </ToolboxProvider>
+  );
+}
 
 function App() {
   return (
@@ -11,7 +31,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} /> 
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:id" element={<HomePage />} /> 
         </Routes>
       </Router>
     </BoardProvider>
