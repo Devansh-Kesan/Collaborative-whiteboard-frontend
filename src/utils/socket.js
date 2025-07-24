1cadd3ef1e1a50bba902a45889a8815ab9222a7d
Devansh Kesan <devanshkesan101@gmail.com>
2025-07-24 20:01:04 +0530

    Updated and added sockets

---
+++ src/utils/socket.js
@@ -0,0 +1,11 @@
+import { io } from "socket.io-client";
+
+const token = localStorage.getItem("whiteboard_user_token");
+console.log(token);
+
+const socket = io("http://localhost:5000", {
+  
+  extraHeaders: token ? { Authorization: `Bearer ${token}` } : {}, // Only send if token exists
+});
+
+export default socket;
